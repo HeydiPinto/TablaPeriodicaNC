58c68fe30dcef08d702253c7faed0b465ed94979
dxrtciyvubon <48489981+dxrtciyvubon@users.noreply.github.com>
2019-05-17 23:35:18 -0500

    agregue las listas a metaloides y a no metales, para poder agregarle datos solo necesito la base da datos lista y ya:)

---
+++ app/src/main/java/com/example/tablaperiodicanc/Emetaloide.java
@@ -0,0 +1,16 @@
+package com.example.tablaperiodicanc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class Emetaloide extends AppCompatActivity {
+
+    ListView jeje;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emetaloide);
+    }
+}
