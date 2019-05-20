e576263f9b89cc0dc2b6355cb5319325c0cf3282
Zulema Jimenez <zcjime02@gmail.com>
2019-05-20 00:44:43 -0500

    activity detalles

---
+++ app/src/main/java/com/example/tablaperiodicanc/Detalle.java
@@ -0,0 +1,20 @@
+package com.example.tablaperiodicanc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Detalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+
+        ((TextView) findViewById(R.id.nombre)).setText("Cobre");
+        ((TextView) findViewById(R.id.dato1)).setText("No.Atomico x");
+        ((TextView) findViewById(R.id.dato2)).setText("Fam. in");
+        ((TextView) findViewById(R.id.dato3)).setText("Otro dato");
+        ((TextView) findViewById(R.id.detalles)).setText("Detale dhfkrbfjkbdfjhvgbkjbdfvb vfkb vuj v rrj bvhb cvj j vj fjj ngf jgk gk gjk gj gj gfj dfgj dfg jdfdgrioyeriañn  ññalsk añlkjh aaña ñandlorem mloe re erljrenmeor ka akaia akai kia kai kai kaia kdbhfvdhgvfujsvdfjsbuhvdgfdhgfvhdjfbsdjbnklsndcd j slhioyeuhje s snushueinsb bae k9ibnikeeeeeeeeeeeeeeeeee hb hbhyjg h   shdhch   bhbhibd vbhi d isi   dsuhiughb jn  kk  akee cixenoxe  xeo  xo x3o 3x ox3 3");
+    }
+}
