27d860157a49e1bd45de6699adb26f94879d6666
Zulema Jimenez <zcjime02@gmail.com>
2019-05-24 03:01:07 -0500

    busqueda_añadiendo

--- app/src/main/java/com/example/tablaperiodicanc/menulateral.java
+++ app/src/main/java/com/example/tablaperiodicanc/menulateral.java
@@ -118,7 +118,7 @@ int i=0;
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+//
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_search) {
             return true;
