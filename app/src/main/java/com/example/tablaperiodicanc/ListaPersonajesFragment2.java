71dd9a9dbcf7c9d262cf8bdbe27589b2d65374bf
reneantoniopalomo <48412939+reneantoniopalomo@users.noreply.github.com>
2019-05-24 00:25:00 -0500

    cambio2

--- app/src/main/java/com/example/tablaperiodicanc/ListaPersonajesFragment2.java
+++ app/src/main/java/com/example/tablaperiodicanc/ListaPersonajesFragment2.java
@@ -69,9 +69,24 @@ public class ListaPersonajesFragment2 extends Fragment {
     }
 
     private void llenarLista() {
-        Listaelementos.add(new clases("Litio", "El litio es un elemento químico de símbolo Li y número atómico 3.", R.drawable.litio));
-        Listaelementos.add(new clases("Berilio", "El berilio es un elemento químico de símbolo Be y número atómico 4.", R.drawable.berilio));
-        Listaelementos.add(new clases("Aluminio", "El aluminio es un elemento químico, de símbolo Al y número atómico 13.", R.drawable.aluminio));
+        Listaelementos.add(new clases("Hidrógeno", "El hidrógeno es el elemento químico de número atómico 1, representado por el símbolo H.", R.drawable.hidrogeno));
+        Listaelementos.add(new clases("Helio", "El helio, por haberse inferido en 1868 su existencia en la atmósfera solar es un elemento químico de número atómico 2, símbolo He y peso atómico estándar de 4,0026.", R.drawable.helio));
+        Listaelementos.add(new clases("Carbono", "El carbono, es un elemento químico con símbolo C, número atómico 6 y masa atómica 12,01. Es un no metal y tetravalente, disponiendo de cuatro electrones para formar enlaces químicos covalentes. ", R.drawable.carbono));
+        Listaelementos.add(new clases("Nitrógeno", "El nitrógeno es un elemento químico de número atómico 7, símbolo N, su masa atómica es de 14,006 u y que en condiciones normales forma un gas diatómico (nitrógeno diatómico o molecular) que constituye del orden del 78 % del aire atmosférico.", R.drawable.nitrogeno));
+        Listaelementos.add(new clases("Oxígeno", "El oxígeno es un elemento químico de número atómico 8 y representado por el símbolo O.", R.drawable.oxigeno));
+        Listaelementos.add(new clases("Flúor", "El flúor es el elemento químico de número atómico 9 situado en el grupo de los halógenos (grupo 17) de la tabla periódica de los elementos.", R.drawable.fluor));
+        Listaelementos.add(new clases("Neón", "El neón es un elemento químico de número atómico 10 y símbolo Ne.", R.drawable.neon));
+        Listaelementos.add(new clases("Fósforo", "El fósforo es un elemento químico de número atómico 15 y símbolo P. ", R.drawable.fosforo));
+        Listaelementos.add(new clases("Azúfre", "El azufre es un elemento químico de número atómico 16 y símbolo S.", R.drawable.azufre));
+        Listaelementos.add(new clases("Cloro", "El cloro es un elemento químico de número atómico 17 situado en el grupo de los halógenos (grupo VIIA) de la tabla periódica de los elementos. ", R.drawable.cloro));
+        Listaelementos.add(new clases("Argón", "El argón es un elemento químico de número atómico 18 y símbolo Ar. Es el tercero de los gases nobles, incoloro e inerte como ellos, constituye el 0,934 % del aire seco.", R.drawable.argon));
+        Listaelementos.add(new clases("Selenio", "El selenio es un elemento químico de la tabla periódica cuyo símbolo es Se y su número atómico 34.", R.drawable.selenio));
+        Listaelementos.add(new clases("Bromo", "El bromo (también llamado antaño fuego líquido) es un elemento químico de número atómico 35 situado en el grupo de los halógenos (grupo VII A) de la tabla periódica de los elementos.", R.drawable.bromo));
+        Listaelementos.add(new clases("Kriptón", "El kriptón o criptón es un elemento químico de la tabla periódica cuyo símbolo es Kr y su número atómico es 36.", R.drawable.kripton));
+        Listaelementos.add(new clases("Yodo", "El yodo o iodo  es un elemento químico de número atómico 53 situado en el grupo de los halógenos (grupo 17) de la tabla periódica de los elementos.", R.drawable.yodo));
+        Listaelementos.add(new clases("Xenón", "El xenón es un elemento químico de la tabla periódica cuyo símbolo es Xe y su número atómico el 54.", R.drawable.xenon));
+        Listaelementos.add(new clases("Radón", "El radón es un elemento químico perteneciente al grupo de los gases nobles.", R.drawable.radon));
+
 
 
     }
